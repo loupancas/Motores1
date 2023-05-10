1b8bc827a40a3ef0bb3bc83435a9f25055ca1f1c
loupancas <129621626+loupancas@users.noreply.github.com>
2023-05-10 00:55:08 -0500

    Danovida

--- Assets/Importstests/Scripts/Weapon/bullet.cs
+++ Assets/Importstests/Scripts/Weapon/bullet.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class bullet : MonoBehaviour
 {
+    public bool isEnemyBullet;
+    public int damage;
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.CompareTag("Enemy"))
@@ -14,7 +16,37 @@ public class bullet : MonoBehaviour
         Destroy(this.gameObject);
     }
 
+    private void OnTriggerEnter(Collider collision) // las naranjas se destruiran cuando colisionen con otros objetos que tengan collider, recordar que deben tener RigidBody
+    {
 
+        var B = collision.gameObject.GetComponent<bullet>();
+
+        if (B != null) // si choca una naranja con otra naranja sale de la función y no lo elimina
+        {
+            return;
+        }
+
+        if (!isEnemyBullet)
+        {
+            Enemy Rompible = collision.gameObject.GetComponent<Enemy>(); // esta trayendo el codigo 
+
+
+            if (Rompible)
+            {
+                Debug.Log("el daño realizado es" + damage);
+                Rompible.golpe(damage); // llama a la funcion golpe del otro script
+
+            }
+
+            if (collision.gameObject.tag != "Player") // se destruirá con todo lo que no sea el PJ o sea los enemigos
+            {
+
+                Destroy(gameObject);
+
+            }
+        }
+
+    }
 
 
 
