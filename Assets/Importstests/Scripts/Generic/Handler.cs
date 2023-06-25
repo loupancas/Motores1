28ae73a1ca388712c979fce26a8288485809da9b
nano2212 <leandrof.mansilla@gmail.com>
2023-06-25 20:11:29 -0300

    Generic

--- Assets/Importstests/Scripts/Generic/Handler.cs
+++ Assets/Importstests/Scripts/Generic/Handler.cs
@@ -8,14 +8,24 @@ public class Handler <T>
     T[] myArray;
     int current = 0;
     
-    public Handler(T[]_array)
+    public Handler(T[] _array)
     {
         myArray = _array;
         current = 0;
 
     }
 
-    public void Nex()
+    public void AddItem(int index, T item)
+    {
+        myArray[index] = item;
+    }
+
+    public void SetNullItem(int index)
+    {
+        myArray[index] = default(T);
+    }
+
+    public void Next()
     {
         current++;
         if(current>=myArray.Length)
@@ -24,12 +34,20 @@ public class Handler <T>
         }
     }
 
+    public T Select(int index)
+    {
+        return myArray[index];
+    }
+
     public T GetCurrent()
     {
         return myArray[current];
     }
 
-
+    public int ArraySize()
+    {
+        return myArray.Length - 1;
+    }
 
 
 }
