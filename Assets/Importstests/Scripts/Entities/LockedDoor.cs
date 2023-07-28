f3e0c5c8b10a42bb6e01149a5333b0baa2cd4d94
patriciomalvasiomaddalena <130863754+patriciomalvasiomaddalena@users.noreply.github.com>
2023-07-28 01:17:11 -0300

    doors!

---
+++ Assets/Importstests/Scripts/Entities/LockedDoor.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockedDoor : MonoBehaviour
+{
+    [Header("references")]
+    GameManager manager;
+    Rigidbody rb;
+
+    [Header("variables")]
+    public bool IsLocked;
+
+    private void Start()
+    {
+        rb = GetComponentInChildren<Rigidbody>();
+        manager = FindObjectOfType<GameManager>();
+    }
+
+
+    public void Unlock()
+    {
+        if(IsLocked == true)
+        {
+            IsLocked= false;
+        }
+
+        UpdateRb();
+    }
+
+    public void Lock()
+    {
+        if(IsLocked== false)
+        {
+            IsLocked= true;
+        }
+
+        UpdateRb();
+    }
+
+    private void UpdateRb()
+    {
+        if(IsLocked== true)
+        {
+            rb.isKinematic= true;
+        }
+        else
+        {
+            rb.isKinematic= false;
+        }
+    }
+
+
+}
